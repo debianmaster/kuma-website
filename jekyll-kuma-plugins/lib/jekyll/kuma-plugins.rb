a42534fafe6d93baa99b785058652f9f6eab6234
Charly Molter <charly.molter@konghq.com>
2023-04-18 16:59:22 +0200

    move policy to plugins
    Signed-off-by: Charly Molter <charly.molter@konghq.com>

--- jekyll-kuma-plugins/lib/jekyll/kuma-plugins.rb
+++ jekyll-kuma-plugins/lib/jekyll/kuma-plugins.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
 
 require_relative "kuma-plugins/version"
-require_relative 'kuma-plugins/liquid/tags/test'
+require_relative 'kuma-plugins/liquid/tags/tabs'
+require_relative 'kuma-plugins/liquid/tags/policyyaml'
